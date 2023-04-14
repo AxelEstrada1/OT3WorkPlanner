4651f4e8c052f36be7820f8da54c8f230f173f5a
Axel Estrada <axelestrada1890@gmail.com>
2023-04-13 19:12:06 -0700

    queryselectorall

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+querySelectorAll()
 
 if (parseInt(document.querySelectorAll(".row")[6].dataset.time) < dayjs().hour()) {
 
